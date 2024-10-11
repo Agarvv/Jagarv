8c40a5cbf207cc527e15bf6fd15d75503b3ac479
Agarvv <casluagarv@gmail.com>
2024-10-11 22:58:09 +0200

    deploy issues

---
+++ client/src/polyfills.ts
@@ -0,0 +1,7 @@
+// This file includes polyfills needed by Angular and is loaded before the app.
+// You can add your own extra polyfills to this file.
+
+import 'zone.js';  // Included with Angular CLI.
+
+(window as any).__Zone_disable_Patch = true; // Disable patching for all objects.
+(window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove']; // Events that should not be patched.
