3b13b88f4b93239ddea294967ce8f568e8a53149
Agarvv <casluagarv@gmail.com>
2024-10-14 23:08:38 +0200

    testing if frontend is ok

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'client';
+  phrase: string = "Working...";
+  ngOnInit(): void {
+    console.log('all works fine')
+  }
 }
