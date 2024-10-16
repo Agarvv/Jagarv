36ab93154772abc4d57698b680a7018cd5f83716
Agarvv <casluagarv@gmail.com>
2024-10-16 23:18:01 +0200

    adding dashboard routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,17 +2,44 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent  } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component'
+import { AdminUsersComponent } from './admin/admin-users/admin-users.component'
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component'
+import { AdminSalesComponent } from './admin/admin-sales/admin-sales.component'
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component'
 
 const routes: Routes = [
-  // THIS IS THE ROUTE FOR THE HOMEPAGE.
+
   {
     path: '',
     component: HomeComponent
   },
-  // THIS IS THE ROUTE FOR THE ADMIN PAGE.
+
   {
     path: 'admin',
-    component: AdminComponent
+    component: AdminComponent,
+    children: [
+      {
+        path: '',
+        component: AdminDashboardComponent
+      },
+      {
+        path: 'users',
+        component: AdminUsersComponent
+      },
+      {
+        path: 'orders',
+        component: AdminOrdersComponent
+      },
+      {
+        path: 'sales',
+        component: AdminSalesComponent
+      },
+      {
+        path: 'products',
+        component: AdminProductsComponent
+      }
+    ]
   }
 ];
 
