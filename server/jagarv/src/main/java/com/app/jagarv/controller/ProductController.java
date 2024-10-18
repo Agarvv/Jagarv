336cc3c666c25197178b34cfdfaa74a316953d5a
Agarvv <casluagarv@gmail.com>
2024-10-18 19:58:46 +0200

    fixing some server import issues

--- server/jagarv/src/main/java/com/app/jagarv/controller/ProductController.java
+++ server/jagarv/src/main/java/com/app/jagarv/controller/ProductController.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/jagarv/products")
--- server/jagarv/src/main/java/com/app/jagarv/dto/ProductDTO.java
+++ server/jagarv/src/main/java/com/app/jagarv/dto/ProductDTO.java
@@ -6,29 +6,70 @@ public class ProductDTO {
     private Long id;
     private String title;
     private String description;
-    private String category;  
+    private String category; 
     private String main_picture;
     private BigDecimal price;
 
+
     public ProductDTO() {
     }
+
 
     public ProductDTO(Long id, String title, String description, String category, String main_picture, BigDecimal price) {
         this.id = id;
         this.title = title;
         this.description = description;
-        this.category = category;  
+        this.category = category; // Asignando la categoría
         this.main_picture = main_picture;
         this.price = price;
     }
 
-
     
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public String getCategory() {
         return category;
     }
 
     public void setCategory(String category) {
         this.category = category;
+    }
+
+    public String getMain_picture() {
+        return main_picture;
+    }
+
+    public void setMain_picture(String main_picture) {
+        this.main_picture = main_picture;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
     }
 }
