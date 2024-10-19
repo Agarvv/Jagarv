67ffb29e1d59d91a416f7c25f30bdafdd9c5fb2d
Agarvv <casluagarv@gmail.com>
2024-10-19 22:51:58 +0200

    adding exception manager

--- server/jagarv/src/main/java/com/app/jagarv/exception/GlobalExceptionHandler.java
+++ server/jagarv/src/main/java/com/app/jagarv/exception/GlobalExceptionHandler.java
@@ -5,14 +5,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-//THIS HANDLER WILL HANDLE OTHER TYPE OF EXCEPTIONS THAT OUR EXCEPTIONS FILES HANDLERS DONT.
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGenericException(Exception ex) {
-        return new ResponseEntity<>("Something went wrong. Please try again later.", HttpStatus.INTERNAL_SERVER_ERROR);
+        
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        ex.printStackTrace(pw);
+        String stackTrace = sw.toString();
+
+       
+        return new ResponseEntity<>(stackTrace, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-    
 }
 
