89a8382f84bb3e8668ce8c60f26bc60339e83868
Agarvv <casluagarv@gmail.com>
2024-10-21 20:38:02 +0200

    i forgot to import multipart on cloudinary service

--- server/jagarv/src/main/java/com/app/jagarv/service/cloudinary/CloudinaryService.java
+++ server/jagarv/src/main/java/com/app/jagarv/service/cloudinary/CloudinaryService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
+import org.springframework.web.multipart.MultipartFile;
 
 // Manages the API's image upload to the cloud
 @Service
