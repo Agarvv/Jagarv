725caf86acc0d976f8efcfc20d8c9a328d843037
Agarvv <casluagarv@gmail.com>
2024-10-21 23:25:18 +0200

    changing some configuratio,

--- server/jagarv/src/main/java/com/app/jagarv/controller/admin/products/ProductController.java
+++ server/jagarv/src/main/java/com/app/jagarv/controller/admin/products/ProductController.java
@@ -6,15 +6,12 @@ import com.app.jagarv.service.admin.product.ProductService;
 import com.app.jagarv.dto.product.CreateProductDTO;
 
 
-import jakarta.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
-import java.util.Map;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.multipart.MultipartFile;
