0d3d45f0e78a6dcbf2a8f10706302787209b63f9
Agarvv <casluagarv@gmail.com>
2024-10-22 23:15:45 +0200

    ISSUES SOLVED, i think :c...

--- client/src/app/environments/environment.ts
+++ client/src/app/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://jagarv-jq5o.onrender.com',
-    cloud_name: 'dtlpkq8w3',
-    upload_preset: 'Jagarv'
-  };
-  
+  production: true,
+  apiUrl: 'https://jagarv-jq5o.onrender.com',
+  cloudinary: {
+      cloud_name: 'dtlpkq8w3',
+      upload_preset: 'Jagarv'
+  }
+};
