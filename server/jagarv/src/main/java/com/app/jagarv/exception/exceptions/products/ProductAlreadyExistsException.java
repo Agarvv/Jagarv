e3cd138bc4dbccd6ce7592e52d1ce831c71cd8c7
Agarvv <casluagarv@gmail.com>
2024-10-24 17:50:48 +0200

    i forgot another time a ;

--- server/jagarv/src/main/java/com/app/jagarv/exception/exceptions/products/ProductAlreadyExistsException.java
+++ server/jagarv/src/main/java/com/app/jagarv/exception/exceptions/products/ProductAlreadyExistsException.java
@@ -2,6 +2,6 @@ package com.app.jagarv.exception.exceptions.product;
 
 public class ProductAlreadyExistsException extends RuntimeException {
     public ProductAlreadyExistsException(String error) {
-        super(error)
+        super(error);
     }
 } // Will be used in the ProductService, in the /create endpoint if the product already exists.
