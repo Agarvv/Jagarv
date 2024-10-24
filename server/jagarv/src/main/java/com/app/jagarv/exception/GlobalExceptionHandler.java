58170d2897fc0b0e8cd27d5fb6fe4340c5020a16
Agarvv <casluagarv@gmail.com>
2024-10-25 00:29:33 +0200

    adding beter error handling in backend

--- server/jagarv/src/main/java/com/app/jagarv/exception/GlobalExceptionHandler.java
+++ server/jagarv/src/main/java/com/app/jagarv/exception/GlobalExceptionHandler.java
@@ -13,14 +13,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGenericException(Exception ex) {
-        
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
         ex.printStackTrace(pw);
         String stackTrace = sw.toString();
 
-       
         return new ResponseEntity<>(stackTrace, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
-
