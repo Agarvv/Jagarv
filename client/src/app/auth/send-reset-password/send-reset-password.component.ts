9ddbe1cab28b22fadefaba209388ab0d2cc0ec71
Agarvv <casluagarv@gmail.com>
2024-11-01 22:08:41 +0100

    fixing troubles

--- client/src/app/auth/send-reset-password/send-reset-password.component.ts
+++ client/src/app/auth/send-reset-password/send-reset-password.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service'; 
 
 
 @Component({
