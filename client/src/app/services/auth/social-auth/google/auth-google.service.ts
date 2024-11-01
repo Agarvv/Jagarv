f8ba7ffd9da2e34c0bee02bdfa6ee8113c36ecde
Agarvv <casluagarv@gmail.com>
2024-11-01 18:49:39 +0100

    saving some changes

--- client/src/app/services/auth/social-auth/google/auth-google.service.ts
+++ client/src/app/services/auth/social-auth/google/auth-google.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs'; 
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGoogleService {
   constructor(private http: HttpClient) { }
-  
-  private apiUrl = "im gonna import environment apiUrl later"
+
+  private apiUrl = environment.apiUrl + '/api/jagarv/auth';
   
   sendGoogleTokenToServer(token: string): Observable<any> {
-      return this.http.post<any>(`${apiUrl}/google`, token); 
+      return this.http.post<any>(`${this.apiUrl}/google`, token); 
   }
 }
