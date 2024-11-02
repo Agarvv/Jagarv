6538f8c419b6bd61628bdcd3309183beb3bb44c6
Agarvv <casluagarv@gmail.com>
2024-11-02 18:33:33 +0100

    solving ussue

--- server/jagarv/src/main/java/com/app/jagarv/outil/SendMail.java
+++ server/jagarv/src/main/java/com/app/jagarv/outil/SendMail.java
@@ -3,10 +3,13 @@ package com.app.jagarv.outil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service 
 public class SendMail {
     @Autowired 
     private JavaMailSender mailSender;
+    
     public void sendMail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
