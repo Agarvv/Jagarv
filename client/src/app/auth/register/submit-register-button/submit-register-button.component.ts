efa6962318a915a653d52a77e166d56d347b15bd
Agarvv <casluagarv@gmail.com>
2024-11-05 22:57:42 +0100

    testing register errr, succes, and loading state if all works propperly

--- client/src/app/auth/register/submit-register-button/submit-register-button.component.ts
+++ client/src/app/auth/register/submit-register-button/submit-register-button.component.ts
@@ -1,30 +1,40 @@
 import { Component, Input } from '@angular/core';
 import { AuthService } from '../../../services/auth/auth.service'; 
-import { FormGroup} from '@angular/forms';
-
+import { FormGroup } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { setLoading, setSuccess, setError, clearMessages } from "../../../store/admin/admin.actions"
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-submit-register-button',
   templateUrl: './submit-register-button.component.html',
-  styleUrl: './submit-register-button.component.css'
+  styleUrls: ['./submit-register-button.component.css' ]  // Corrección aquí
 })
 export class SubmitRegisterButtonComponent {
    @Input() form!: FormGroup;
     
-   constructor(private authService: AuthService) {
-       
-   }
+   constructor(private authService: AuthService, private store: Store) {}
    
    submitForm() {
+     this.store.dispatch(clearMessages());
        if(this.form.invalid) {
            this.form.markAllAsTouched();
-           return 
+           return; 
        }
+
+       this.store.dispatch(setLoading({ isLoading: true}));
        
-       this.authService.registerUser(this.form.value).subscribe((data) => {
-           console.log("All Ok", data)
-       }, (error) => {
-           console.error("ERR REGISTER", error)
-       })
+       this.authService.registerUser(this.form.value).pipe( 
+         finalize(() => {
+           this.store.dispatch(setLoading({ isLoading: false })); 
+         })
+       ).subscribe(
+         (data) => {
+           this.store.dispatch(setSuccess({ successMessage: "Welcome To Jagarv! Please Wait While We Redirect You..."}));
+         },
+         (error) => {
+           this.store.dispatch(setError({ errorMessage: error.error}));
+         }
+       );
    }
 }
