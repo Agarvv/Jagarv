8e2c7a18deaa78b4d1c327cc6df26564ebf4adf9
Agarvv <casluagarv@gmail.com>
2024-11-06 18:57:50 +0100

    internal pointer variable :p

--- server/jagarv/src/main/java/com/app/jagarv/outil/CookieOutil.java
+++ server/jagarv/src/main/java/com/app/jagarv/outil/CookieOutil.java
@@ -4,13 +4,12 @@ import jakarta.servlet.http.Cookie;
 
 public class CookieOutil {
     public static Cookie generateJwtCookie(String jwtToken) {
-        Cookie cookie = new Cookie("jwt", jwtToken);  
-        cookie.setHttpOnly(true); 
-        cookie.setSecure(true); 
-        cookie.setPath("/"); 
-        cookie.setMaxAge(3600); 
-        cookie.setDomain("jagarv.vercel.app");
-
+        Cookie cookie = new Cookie("jwt", jwtToken);
+        cookie.setHttpOnly(true);  
+        cookie.setSecure(true);    
+        cookie.setPath("/");       
+        cookie.setMaxAge(3600);    
+        cookie.setDomain("jagarv-jq5o.onrender.com"); 
         cookie.setAttribute("SameSite", "None");  
 
         return cookie;
