7953bcdcdfb7e41bae046eacc294b8f71f08ba20
Agarvv <casluagarv@gmail.com>
2024-11-06 01:32:47 +0100

    as always i forgot something.

--- client/src/app/auth/reset-password/reset-password-button/reset-password-button.component.ts
+++ client/src/app/auth/reset-password/reset-password-button/reset-password-button.component.ts
@@ -1,47 +1,68 @@
 import { Component, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms'
+import { FormGroup } from '@angular/forms';
 import { AuthService } from '../../../services/auth/auth.service';
 import { Store } from '@ngrx/store';
-import { setLoading, setError, setSuccess, clearMessages } from '../../../store/admin/admin.actions'
+import { setLoading, setError, setSuccess, clearMessages } from '../../../store/admin/admin.actions';
 import { finalize } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password-button',
   templateUrl: './reset-password-button.component.html',
-  styleUrl: './reset-password-button.component.css'
+  styleUrls: ['./reset-password-button.component.css']
 })
 export class ResetPasswordButtonComponent {
   @Input() form: FormGroup | null = null;
+  private email: string | null = null;
+  private token: string | null = null;
 
-
- constructor(private authService: AuthService, private store: Store) {}
-
- sendResetPassword() { 
-     this.store.dispatch(clearMessages());
-     
-  if(!this.form) {
-    return;  // Form not provided, do nothing and return early.
+  constructor(
+    private authService: AuthService,
+    private store: Store,
+    private route: ActivatedRoute  
+  ) {
+    
+    this.email = this.route.snapshot.queryParamMap.get('email');
+    this.token = this.route.snapshot.queryParamMap.get('token');
   }
+
+  sendResetPassword() { 
+    this.store.dispatch(clearMessages());
+     
+    if (!this.form) {
+      return;  // Form not provided, do nothing and return early.
+    }
   
-   if (this.form && this.form.invalid) {
-     this.form.markAllAsTouched();
-     return;
-   }
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    
+    if (!this.email || !this.token) {
+      console.error('Email or token is missing from the route parameters');
+      return;
+    }
+    
+    this.store.dispatch(setLoading({ isLoading: true }));
+    
+    const requestData = {
+      email: this.email,
+      token: this.token,
+      password: this.form.value.password
+    };
 
-   this.authService.sendResetPassword(this.form.value).pipe(
-    finalize(() => {
-        this.store.dispatch(setLoading({ isLoading: false }))
-    })
-   ).subscribe((data) => {
-       this.store.dispatch(setSuccess({ successMessage: 'Your Password Has Been Reset!' }));
-   }, (error) => {
-       this.store.dispatch(setError({ errorMessage: error.error }));
+    this.authService.sendResetPassword(requestData).pipe(
+      finalize(() => {
+        this.store.dispatch(setLoading({ isLoading: false }));
+      })
+    ).subscribe(
+      (data) => {
+        this.store.dispatch(setSuccess({ successMessage: 'Your Password Has Been Reset!' }));
+      },
+      (error) => {
+        this.store.dispatch(setError({ errorMessage: error.error }));
         console.error('Error sending reset password', error); 
-   })
-   
-   
-   
- }
-
-
+      }
+    );
+  }
 }
