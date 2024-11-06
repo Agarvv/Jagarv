14f3e2da14e571d0ec7bae2672d3c11cf4d2d6e1
Agarvv <casluagarv@gmail.com>
2024-11-06 18:53:22 +0100

    im having some issues.

--- server/jagarv/src/main/java/com/app/jagarv/outil/CookieOutil.java
+++ server/jagarv/src/main/java/com/app/jagarv/outil/CookieOutil.java
@@ -5,13 +5,14 @@ import jakarta.servlet.http.Cookie;
 public class CookieOutil {
     public static Cookie generateJwtCookie(String jwtToken) {
         Cookie cookie = new Cookie("jwt", jwtToken);  
-        cookie.setSecure(true);
-        cookie.setHttpOnly(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(3600);
+        cookie.setHttpOnly(true); 
+        cookie.setSecure(true); 
+        cookie.setPath("/"); 
+        cookie.setMaxAge(3600); 
         cookie.setDomain("jagarv.vercel.app");
+
         cookie.setAttribute("SameSite", "None");  
-        
+
         return cookie;
     }
 }
