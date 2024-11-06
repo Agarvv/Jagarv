306851a5429baf977c2a5bb17e0c6e69746065a2
Agarvv <casluagarv@gmail.com>
2024-11-06 22:23:41 +0100

    error with google auth in backend

--- server/jagarv/src/main/java/com/app/jagarv/exception/GlobalExceptionHandler.java
+++ server/jagarv/src/main/java/com/app/jagarv/exception/GlobalExceptionHandler.java
@@ -11,14 +11,14 @@ import java.io.StringWriter;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    // @ExceptionHandler(Exception.class)
-    // public ResponseEntity<String> handleGenericException(Exception ex) {
-    //    StringWriter sw = new StringWriter();
-   //    PrintWriter pw = new PrintWriter(sw);
-   //   ex.printStackTrace(pw);
- //   String stackTrace = sw.toString();
+     @ExceptionHandler(Exception.class)
+     public ResponseEntity<String> handleGenericException(Exception ex) {
+        StringWriter sw = new StringWriter();
+      PrintWriter pw = new PrintWriter(sw);
+      ex.printStackTrace(pw);
+      String stackTrace = sw.toString();
 
-  //    return new ResponseEntity<>(stackTrace, HttpStatus.INTERNAL_SERVER_ERROR);
-  //  }
+     return new ResponseEntity<>(stackTrace, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   
 }
