4d2c70351f4c6a4c3d098b8ec8c101e41daf4718
Agarvv <casluagarv@gmail.com>
2024-11-11 22:48:23 +0100

    get product implemented on product pageé

--- client/src/app/product-details/product-details.component.ts
+++ client/src/app/product-details/product-details.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { PublicProductsService } from '@services/products/products.service';
+import { Product } from "@models/Product"
+import { ActivatedRoute } from '@angular/router'; 
+import { Store } from '@ngrx/store';
+import { setError, clearMessages } from "@store/admin/admin.actions"
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
+// this component gets the product wanted
 export class ProductDetailsComponent {
+   product: Product | undefined; // this will hold the product details
 
+   constructor(
+    private productsService: PublicProductsService,
+    private route: ActivatedRoute,
+    private store: Store
+    ) {}
+   
+    // gets the product and sets it
+   getProduct(): void {
+    this.store.dispatch(clearMessages());
+    const productId = this.route.snapshot.paramMap.get('productId')
+     this.productsService.getProductById(Number(productId)).subscribe((product: Product) => {
+       this.product = product;
+       console.log('product received', product) // debug
+     }, (error) => {
+      console.error('error', error) // debug for developers
+        this.store.dispatch(setError({ errorMessage: "Something went wrong. Please Try again later" }));
+         // quick error mesage
+     });
+   }
 }
