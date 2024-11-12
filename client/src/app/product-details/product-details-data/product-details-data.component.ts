a0ef5191fd7fc4f72e5beb473a87b085cd434461
Agarvv <casluagarv@gmail.com>
2024-11-12 02:23:26 +0100

    debugging, debugging and debuging

--- client/src/app/product-details/product-details-data/product-details-data.component.ts
+++ client/src/app/product-details/product-details-data/product-details-data.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '@models/Product';
 import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
@@ -9,10 +9,17 @@ import { ProductDetailsState } from '@store/cart/product-details.state';
   templateUrl: './product-details-data.component.html',
   styleUrls: ['./product-details-data.component.css'] 
 })
-export class ProductDetailsDataComponent {
+export class ProductDetailsDataComponent implements OnInit {
   product$: Observable<Product | null>;
 
   constructor(private store: Store<ProductDetailsState>) {
     this.product$ = this.store.pipe(select(state => state.product));
+  }
+
+  // debug
+  ngOnInit(): void {
+    this.product$.subscribe(product => {
+      console.log('Product received updated', product);
+    });
   }
 }
