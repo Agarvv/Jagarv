fa89b9df4132049ed89b025ee54e029475c04657
Agarvv <casluagarv@gmail.com>
2024-11-12 02:47:44 +0100

    idk what is wronh.

--- client/src/app/product-details/product-details-data/product-details-data.component.ts
+++ client/src/app/product-details/product-details-data/product-details-data.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '@models/Product';
 import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { ProductDetailsState } from '@store/cart/product-details.state';
+import { tap } from 'rxjs/operators';  // No olvides importar tap
 
 @Component({
   selector: 'app-product-details-data',
   templateUrl: './product-details-data.component.html',
   styleUrls: ['./product-details-data.component.css']
 })
-export class ProductDetailsDataComponent {
+export class ProductDetailsDataComponent implements OnInit {
   product$: Observable<Product | null>;
 
   constructor(private store: Store<ProductDetailsState>) {
-    this.product$ = this.store.pipe(select(state => state.productDetails.product));
+
+    this.product$ = this.store.pipe(
+      select(state => state.product),  
+      tap(product => console.log('Product updated', product)) 
+    );
   }
 
 }
