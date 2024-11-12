648c72b2610542991117336007814f18d08a770e
Agarvv <casluagarv@gmail.com>
2024-11-12 02:40:32 +0100

    el error estaba siempre en mis narizes creo.

--- client/src/app/product-details/product-details-data/product-details-data.component.ts
+++ client/src/app/product-details/product-details-data/product-details-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Product } from '@models/Product';
 import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
@@ -7,20 +7,13 @@ import { ProductDetailsState } from '@store/cart/product-details.state';
 @Component({
   selector: 'app-product-details-data',
   templateUrl: './product-details-data.component.html',
-  styleUrls: ['./product-details-data.component.css'] 
+  styleUrls: ['./product-details-data.component.css']
 })
-export class ProductDetailsDataComponent implements OnInit {
+export class ProductDetailsDataComponent {
   product$: Observable<Product | null>;
 
   constructor(private store: Store<ProductDetailsState>) {
-    this.product$ = this.store.pipe(select(state => state.product));
+    this.product$ = this.store.pipe(select(state => state.productDetails.product));
   }
-
-  // debug
-  ngOnInit() {
-  this.store.pipe(select(state => state.productDetails.product)).subscribe(product => {
-    console.log('Store producg:', product);
-  });
-}
 
 }
