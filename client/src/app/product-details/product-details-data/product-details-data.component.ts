d5ed7520f3f3955f6889fc39fcb07fa2169f02b6
Agarvv <casluagarv@gmail.com>
2024-11-12 02:49:50 +0100

    issue

--- client/src/app/product-details/product-details-data/product-details-data.component.ts
+++ client/src/app/product-details/product-details-data/product-details-data.component.ts
@@ -3,22 +3,23 @@ import { Product } from '@models/Product';
 import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { ProductDetailsState } from '@store/cart/product-details.state';
-import { tap } from 'rxjs/operators';  // No olvides importar tap
 
 @Component({
   selector: 'app-product-details-data',
   templateUrl: './product-details-data.component.html',
-  styleUrls: ['./product-details-data.component.css']
+  styleUrls: ['./product-details-data.component.css'] 
 })
 export class ProductDetailsDataComponent implements OnInit {
   product$: Observable<Product | null>;
 
   constructor(private store: Store<ProductDetailsState>) {
-
-    this.product$ = this.store.pipe(
-      select(state => state.product),  
-      tap(product => console.log('Product updated', product)) 
-    );
+    this.product$ = this.store.pipe(select(state => state.product));
   }
+  
+  ngOnInit() {
+      console.log('Debug, Java >>> C#')
+  }
+
+}
 
 }
