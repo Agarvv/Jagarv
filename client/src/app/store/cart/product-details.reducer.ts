8b86ef724e973c0a828e4760ca65497d946e549b
Agarvv <casluagarv@gmail.com>
2024-11-12 02:18:04 +0100

    not going to sleep... :c

--- client/src/app/store/cart/product-details.reducer.ts
+++ client/src/app/store/cart/product-details.reducer.ts
@@ -1,11 +1,14 @@
 import { createReducer, on } from '@ngrx/store';
-import { setProduct } from './product-details.actions'; // Importar la acción
+import { setProduct } from './product-details.actions';
 import { ProductDetailsState, initialState } from './product-details.state';
 
 export const productDetailsReducer = createReducer(
   initialState,
-  on(setProduct, (state, { product }) => ({
-    ...state,  
-    product,   
-  }))
+  on(setProduct, (state, { product }) => {
+    console.log('Product set in reducer:', product); // debug
+    return {
+      ...state,
+      product,
+    };
+  })
 );
