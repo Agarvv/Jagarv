b8b98dd18e1bf7c4d0a6b6138726839a3128b7dd
Agarvv <casluagarv@gmail.com>
2024-11-14 14:03:24 +0100

    Add to cart functionality fullstack implemented

--- client/src/app/product-details/add-to-cart-button/add-to-cart-button.component.ts
+++ client/src/app/product-details/add-to-cart-button/add-to-cart-button.component.ts
@@ -1,10 +1,70 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { ProductDetailsState } from '@store/cart/product-details.state';
+import { Product } from '@models/Product';
+import { CartService } from "@services/cart/cart.service"
+
 
 @Component({
   selector: 'app-add-to-cart-button',
   templateUrl: './add-to-cart-button.component.html',
-  styleUrl: './add-to-cart-button.component.css'
+  styleUrls: ['./add-to-cart-button.component.css']
 })
 export class AddToCartButtonComponent {
+  addProductForm: FormGroup;
+  product$: Observable<Product | null>;
+  options$: Observable<{ attributeName: string; attributeOptionId: number }[]> | null;
 
+  constructor(
+    private fb: FormBuilder,
+    private cartService: CartService,
+    private store: Store<ProductDetailsState>
+  ) {
+    this.product$ = this.store.pipe(select((state: any) => state.productDetails.product));
+    this.options$ = this.store.pipe(select((state: any) => state.productDetails.attributes));
+
+    this.addProductForm = this.fb.group({
+      productId: ['', Validators.required],
+      options: this.fb.array([], Validators.required)  
+    });
+
+    this.options$.subscribe(options => {
+      const optionIds = options?.map(option => option.attributeOptionId) || [];
+      this.setOptions(optionIds);
+    });
+
+    this.product$.subscribe(product => {
+      if (product) {
+        this.addProductForm.patchValue({
+          productId: product.id
+        });
+      }
+    });
+  }
+
+  private setOptions(optionIds: number[]) {
+    const optionsArray = this.addProductForm.get('options') as FormArray;
+    optionsArray.clear(); 
+    
+    optionIds.forEach(id => {
+      optionsArray.push(this.fb.control(id));
+    });
+  }
+  
+  addToCart(): void {
+      console.log('value', this.addProductForm.value)
+      if(this.addProductForm.invalid) {
+          console.error("Form not valid")
+          return 
+      }
+      
+      this.cartService.addToCard(this.addProductForm.value).subscribe((data) => {
+          console.log("Added to cart") // debug
+      }, (error) => {
+          console.error(error); // debug
+      })
+  }
+  
 }
--- client/src/app/services/cart/cart.service.ts
+++ client/src/app/services/cart/cart.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AddToCart } from "@models/cart/AddToCart"
+import { environment } from "@env/environment"
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+    
+  private apiUrl = `${apiUrl}/api/jagarv/cart`
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  addToCard(data: AddToCart): Observable<AddToCart> 
+  {
+      return this.http.post(`${apiUrl}/create`, data);
+  }
+  
+  
 }
