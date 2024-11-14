34a94f8a6bf995b9235876c80b0e9a9b6dd5db65
Agarvv <casluagarv@gmail.com>
2024-11-14 02:24:48 +0100

    all should work fine now

--- client/src/app/product-details/create-product-opinion/create-opinion-submit/create-opinion-submit.component.ts
+++ client/src/app/product-details/create-product-opinion/create-opinion-submit/create-opinion-submit.component.ts
@@ -1,43 +1,45 @@
 import { Component, Input } from '@angular/core';
-import { FormGroup } from "@angular/forms"
-import { OpinionsService } from '@services/product/opinions/opinions.service'
+import { FormGroup } from '@angular/forms';
+import { OpinionsService } from '@services/products/opinions/opinions.service';
 import { finalize } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { setLoading, setError, clearMessages } from '@store/admin/admin.actions';
 
-
 @Component({
   selector: 'app-create-opinion-submit',
   templateUrl: './create-opinion-submit.component.html',
-  styleUrl: './create-opinion-submit.component.css'
+  styleUrls: ['./create-opinion-submit.component.css']
 })
 export class CreateOpinionSubmitComponent {
-   @Input() form: FormGroup | null; 
+   @Input() form!: FormGroup; 
    
-   constructor(opinionsService: OpinionsService, private store: Store) {} 
-    
+   constructor(
+     private opinionsService: OpinionsService,  
+     private store: Store
+   ) {}
+
   createOpinion(): void {
-      this.store.dispatch(clearMessages()); 
-      
-      if(this.form.invalid) {
-          this.form.markAllAsTouched();
-          return 
+      this.store.dispatch(clearMessages());
+
+      if (this.form?.invalid) { 
+          this.form?.markAllAsTouched();
+          return;
       }
-      
-      this.store.dispatch(setLoading({ isLoading: true }))
-      
+
+      this.store.dispatch(setLoading({ isLoading: true }));
+
       this.opinionsService.createOpinion(this.form.value).pipe(
         finalize(() => {
-            this.store.dispatch(setLoading({ isLoading: false }))
+            this.store.dispatch(setLoading({ isLoading: false }));
         })
-      ).subscribe((data) => {
-          console.log('Opinion created', data) // debug
-          
-          // just for now 
-          window.location.reload();
-      }, (error) => {
-          this.store.dispatch(setError({ errorMessage: error.error}))
-      })
+      ).subscribe(
+        (data: any) => {  
+            console.log('Opinion created', data);
+            window.location.reload();  // jusf for now
+        }, 
+        (error: any) => {  
+            this.store.dispatch(setError({ errorMessage: error.error }));
+        }
+      );
   }
-  
 }
