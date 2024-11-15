6d9f4f2d86580191d1257d000e715900dbdd0e33
Agarvv <casluagarv@gmail.com>
2024-11-15 21:22:45 +0100

    issues

--- client/src/app/services/payments/stripe/stripe.service.ts
+++ client/src/app/services/payments/stripe/stripe.service.ts
@@ -8,22 +8,29 @@ import { environment } from '@env/environment';
 export class StripeService {
   private stripe: Stripe | null = null;
   private elements: StripeElements | null = null;
+  private initialized: Promise<void>;
 
-  async initStripe(): Promise<void> {
-    if (!this.stripe) {
-      const stripe = await loadStripe(environment.stripePublicKey);
-      if (stripe) {
-        this.stripe = stripe;
-        this.elements = stripe.elements();
-      } else {
-        throw new Error('Stripe could not init'); // debug
-      }
+  constructor() {
+    this.initialized = this.initializeStripe();
+  }
+
+  private async initializeStripe(): Promise<void> {
+    const stripe = await loadStripe(environment.stripePublicKey);
+    if (!stripe) {
+      throw new Error('could not init stripe');
     }
+    this.stripe = stripe;
+    this.elements = stripe.elements();
+  }
+
+  async ensureInitialized(): Promise<void> {
+    return this.initialized; 
   }
 
   async createPaymentMethod(cardElement: any): Promise<any> {
+    await this.ensureInitialized();
     if (!this.stripe) {
-      throw new Error('Stripe not initzialized'); // debug
+      throw new Error('Stripe not init');
     }
     return this.stripe.createPaymentMethod({
       type: 'card',
@@ -31,9 +38,10 @@ export class StripeService {
     });
   }
 
-  createCardElement() {
+  async createCardElement() {
+    await this.ensureInitialized();
     if (!this.elements) {
-      throw new Error('Stripe Elements not initzialized'); // debug
+      throw new Error('Stripe Elements not init');
     }
     return this.elements.create('card');
   }
