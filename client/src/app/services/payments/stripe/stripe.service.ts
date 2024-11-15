e5e00a830631d08a777147ef41bf7782706d791f
Agarvv <casluagarv@gmail.com>
2024-11-15 21:19:39 +0100

    issues

--- client/src/app/services/payments/stripe/stripe.service.ts
+++ client/src/app/services/payments/stripe/stripe.service.ts
@@ -9,18 +9,21 @@ export class StripeService {
   private stripe: Stripe | null = null;
   private elements: StripeElements | null = null;
 
-  constructor() {
-    loadStripe(environment.stripePublicKey).then((stripe: Stripe | null) => {
-      if (stripe) { 
+  async initStripe(): Promise<void> {
+    if (!this.stripe) {
+      const stripe = await loadStripe(environment.stripePublicKey);
+      if (stripe) {
         this.stripe = stripe;
         this.elements = stripe.elements();
+      } else {
+        throw new Error('Stripe could not init'); // debug
       }
-    });
+    }
   }
 
   async createPaymentMethod(cardElement: any): Promise<any> {
     if (!this.stripe) {
-      throw new Error('Stripe is not initzialized');
+      throw new Error('Stripe not initzialized'); // debug
     }
     return this.stripe.createPaymentMethod({
       type: 'card',
@@ -30,7 +33,7 @@ export class StripeService {
 
   createCardElement() {
     if (!this.elements) {
-      throw new Error('Stripe Elements not initialized');
+      throw new Error('Stripe Elements not initzialized'); // debug
     }
     return this.elements.create('card');
   }
