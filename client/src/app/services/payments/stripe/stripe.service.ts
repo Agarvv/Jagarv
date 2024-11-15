6d85a2260643a5b303bec2cd53ecac6f7367511f
Agarvv <casluagarv@gmail.com>
2024-11-15 21:07:45 +0100

    issues fixed

--- client/src/app/services/payments/stripe/stripe.service.ts
+++ client/src/app/services/payments/stripe/stripe.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { loadStripe } from '@stripe/stripe-js';
-import { environment } from 'src/environments/environment';
+import { environment } from '@env/environment';
 
 @Injectable({
   providedIn: 'root',
