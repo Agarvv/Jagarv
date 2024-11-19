dc68a5c9de4de73bca401ada441d0aa68f1e7f14
Agarvv <casluagarv@gmail.com>
2024-11-19 15:14:40 +0100

    Issues

--- server/jagarv/src/main/java/com/app/jagarv/mapper/product/ProductOpinionMapper.java
+++ server/jagarv/src/main/java/com/app/jagarv/mapper/product/ProductOpinionMapper.java
@@ -2,10 +2,11 @@ package com.app.jagarv.mapper.product;
 
 import com.app.jagarv.dto.product.read.ProductOpinionDTO;
 import com.app.jagarv.entity.product.ProductOpinion;
+import com.app.jagarv.mapper.user.UserMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {UserMapper.class})
 public interface ProductOpinionMapper {
 
     ProductOpinionMapper INSTANCE = Mappers.getMapper(ProductOpinionMapper.class);
