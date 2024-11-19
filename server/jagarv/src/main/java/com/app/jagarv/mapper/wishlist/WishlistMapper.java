8767b99a7895eefc3418c950f1ade2042fbb50a1
Agarvv <casluagarv@gmail.com>
2024-11-19 18:56:37 +0100

    Fixing some bug

--- server/jagarv/src/main/java/com/app/jagarv/mapper/wishlist/WishlistMapper.java
+++ server/jagarv/src/main/java/com/app/jagarv/mapper/wishlist/WishlistMapper.java
@@ -1,10 +1,22 @@
 package com.app.jagarv.mapper.wishlist;
 
 import com.app.jagarv.dto.wishlist.WishlistDTO;
+import com.app.jagarv.dto.product.read.ProductDTO;
 import com.app.jagarv.entity.wishlist.Wishlist;
+import com.app.jagarv.entity.product.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface WishlistMapper {
+
+    WishlistMapper INSTANCE = Mappers.getMapper(WishlistMapper.class);
+
+    @Mapping(source = "products", target = "products")
     WishlistDTO toDto(Wishlist wishlist);
+    
+    ProductDTO productToProductDTO(Product product);
 }
