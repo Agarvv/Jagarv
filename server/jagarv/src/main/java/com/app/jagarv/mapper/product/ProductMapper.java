3591dc06a9ef542c4fd570e22efff569bab7478e
Agarvv <casluagarv@gmail.com>
2024-11-19 15:42:49 +0100

    issues

--- server/jagarv/src/main/java/com/app/jagarv/mapper/product/ProductMapper.java
+++ server/jagarv/src/main/java/com/app/jagarv/mapper/product/ProductMapper.java
@@ -2,19 +2,23 @@ package com.app.jagarv.mapper.product;
 
 import com.app.jagarv.dto.product.read.ProductDTO;
 import com.app.jagarv.entity.product.Product;
+import com.app.jagarv.mapper.user.UserMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring", uses = {ProductCategoryMapper.class, ProductVariantMapper.class, ProductOpinionMapper.class})
+@Mapper(componentModel = "spring", uses = {ProductCategoryMapper.class, ProductVariantMapper.class, UserMapper.class})
 public interface ProductMapper {
+
+    ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
 
     @Mapping(source = "category", target = "category")
     @Mapping(source = "variants", target = "variants")
-    @Mapping(source = "opinions", target = "opinions") 
+    @Mapping(source = "opinions", target = "opinions")
     ProductDTO productToDTO(Product product);
 
     @Mapping(source = "category", target = "category")
     @Mapping(source = "variants", target = "variants")
-    @Mapping(source = "opinions", target = "opinions") 
+    @Mapping(source = "opinions", target = "opinions")
     Product dtoToProduct(ProductDTO productDTO);
 }
