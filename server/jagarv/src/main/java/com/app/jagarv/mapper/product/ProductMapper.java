dd4e096bd340ebbaaff2b8f4c2649707ac7089ba
Agarvv <casluagarv@gmail.com>
2024-11-19 15:29:03 +0100

    issues

--- server/jagarv/src/main/java/com/app/jagarv/mapper/product/ProductMapper.java
+++ server/jagarv/src/main/java/com/app/jagarv/mapper/product/ProductMapper.java
@@ -4,20 +4,17 @@ import com.app.jagarv.dto.product.read.ProductDTO;
 import com.app.jagarv.entity.product.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring", uses = {ProductCategoryMapper.class, ProductVariantMapper.class, ProductOpinionMapper.class})
 public interface ProductMapper {
 
-    ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
-
     @Mapping(source = "category", target = "category")
     @Mapping(source = "variants", target = "variants")
-    @Mapping(source = "opinions", target = "opinions")  
+    @Mapping(source = "opinions", target = "opinions") 
     ProductDTO productToDTO(Product product);
 
     @Mapping(source = "category", target = "category")
     @Mapping(source = "variants", target = "variants")
-    @Mapping(source = "opinions", target = "opinions")  
+    @Mapping(source = "opinions", target = "opinions") 
     Product dtoToProduct(ProductDTO productDTO);
 }
--- server/jagarv/src/main/java/com/app/jagarv/mapper/product/ProductOpinionMapper.java
+++ server/jagarv/src/main/java/com/app/jagarv/mapper/product/ProductOpinionMapper.java
@@ -6,10 +6,16 @@ import com.app.jagarv.mapper.user.UserMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import com.app.jagarv.dto.user.UserDTO;
+import com.app.jagarv.entity.user.User;
+
 @Mapper(componentModel = "spring", uses = {UserMapper.class})
 public interface ProductOpinionMapper {
 
     ProductOpinionMapper INSTANCE = Mappers.getMapper(ProductOpinionMapper.class);
-
+    
+    @Mapping(source = "user", target = "user")
     ProductOpinionDTO productOpinionToDTO(ProductOpinion opinion);
+    
+    UserDTO userToDTO(User user); 
 }
