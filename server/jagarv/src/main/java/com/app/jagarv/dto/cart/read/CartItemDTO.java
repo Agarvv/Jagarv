b2f095c2299b38193d4917fa74393939968077fc
Agarvv <casluagarv@gmail.com>
2024-11-19 16:27:07 +0100

    issues

--- server/jagarv/src/main/java/com/app/jagarv/dto/cart/read/CartItemDTO.java
+++ server/jagarv/src/main/java/com/app/jagarv/dto/cart/read/CartItemDTO.java
@@ -3,7 +3,6 @@ package com.app.jagarv.dto.cart.read;
 import java.util.List;
 import java.math.BigDecimal; 
 
-
 public class CartItemDTO {
 
     private Long productId;
@@ -11,14 +10,17 @@ public class CartItemDTO {
     private BigDecimal price; 
     private Long quantity;
     private List<Long> options;
+    private List<String> pictures; 
 
-    public CartItemDTO(Long productId, String productName, Long quantity, List<Long> options) {
+    public CartItemDTO(Long productId, String title, BigDecimal price, Long quantity, List<Long> options, List<String> pictures) {
         this.productId = productId;
-        this.title  = productName;
+        this.title = title;
+        this.price = price; 
         this.quantity = quantity;
         this.options = options;
+        this.pictures = pictures; 
     }
-
+    
     public Long getProductId() {
         return productId;
     }
@@ -57,5 +59,13 @@ public class CartItemDTO {
 
     public void setPrice(BigDecimal price) {
         this.price = price;
+    }
+    
+    public List<String> getPictures() {
+        return pictures; 
+    }
+    
+    public void setPictures(List<String> pictures) {
+        this.pictures = pictures; 
     }
 }
