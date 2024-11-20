f9d1fe736c79de1436c2987803df84bc57db27d4
Agarvv <casluagarv@gmail.com>
2024-11-20 22:18:52 +0100

    issues

--- client/src/app/layout/search-page-aside/search-page-aside.component.ts
+++ client/src/app/layout/search-page-aside/search-page-aside.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { SearchState } from '@store/search/search.state'; 
 import { Product } from '@models/Product';  
+import { Filters } from '@models/search/Filters';
 
 @Component({
   selector: 'app-search-page-aside',
@@ -13,7 +14,13 @@ export class SearchPageAsideComponent implements OnInit {
 
   searchResults$: Observable<Product[]> = new Observable<Product[]>();
 
-  filters = {
+  filters: Filters = {
+    category: '',
+    minPrice: null,
+    maxPrice: null,
+    ram: null,
+    storage: null,
+    color: null,
 
   };
 
---
+++ client/src/app/models/search/Filters.ts
@@ -0,0 +1,8 @@
+export interface Filters {
+        category: string;
+        minPrice: number | null;
+        maxPrice: number | null;
+        ram: string | null;
+        storage: string | null;
+        color: string | null;
+}
