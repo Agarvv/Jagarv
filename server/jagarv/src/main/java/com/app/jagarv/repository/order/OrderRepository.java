326ac2eb0aab273c0fbf6fa479a555b244950f5a
Agarvv <casluagarv@gmail.com>
2024-11-22 00:57:19 +0100

    issues

--- server/jagarv/src/main/java/com/app/jagarv/repository/order/OrderRepository.java
+++ server/jagarv/src/main/java/com/app/jagarv/repository/order/OrderRepository.java
@@ -1,11 +1,8 @@
 package com.app.jagarv.repository.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.app.jagarv.entity.order.Order;
 
-
-
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    Boolean existsByUserIdAndProductId(Long userId, Long productId);
+    Boolean existsByUserIdAndProducts_Id(Long userId, Long productId);
 }
