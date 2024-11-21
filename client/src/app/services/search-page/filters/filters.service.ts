526a7b5d09dfe4143c26c85e9c7d295cc98b253a
Agarvv <casluagarv@gmail.com>
2024-11-21 21:13:33 +0100

    improving filters system

--- client/src/app/services/search-page/filters/filters.service.ts
+++ client/src/app/services/search-page/filters/filters.service.ts
@@ -14,12 +14,31 @@ export class FiltersService {
     console.log('filters', filters);
 
     return products.filter((product) => {
-      return (!filters.category || product.category.name === filters.category) &&
-             (!filters.minPrice || product.price >= filters.minPrice) &&
-             (!filters.maxPrice || product.price <= filters.maxPrice) &&
-             (!filters.color || product.category.attributes.some
+      return (!filters.category || product.category.name === filters.category) && // category filter
+
+             (!filters.minPrice || product.price >= filters.minPrice) && // min price filter 
+
+             (!filters.maxPrice || product.price <= filters.maxPrice) && // max price filter
+
+             (!filters.color || product.category.attributes.some // color filter
               (attr => attr.name === 'Color' && attr.options.some
-                (option => option.value.toLowerCase() === filters.color?.toLowerCase())));
+                (option => option.value.toLowerCase() === filters.color?.toLowerCase()))) &&
+
+                // storage filter
+                (!filters.storage || product.category.attributes.some(
+                  attr => attr.name == 'Storage' && attr.options.some(
+                    option => option.value.toLowerCase() == filters.storage?.toLowerCase()
+                  )
+                )) &&
+
+                // ram filter 
+                (!filters.ram || product.category.attributes.some(attr =>
+                  attr.name == 'RAM' && attr.options.some(option => 
+                    option.value.toLowerCase() == filters.ram?.toLowerCase()
+                  )
+                ));
+
+                // i will add here more if necesary
     });
   }
 }
