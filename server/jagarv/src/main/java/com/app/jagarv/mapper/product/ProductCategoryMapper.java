16f491c92379522f46fb13c5a75c9b98ceb28b70
Agarvv <casluagarv@gmail.com>
2024-11-22 15:49:06 +0100

    Render deploy issues

--- server/jagarv/src/main/java/com/app/jagarv/mapper/product/ProductCategoryMapper.java
+++ server/jagarv/src/main/java/com/app/jagarv/mapper/product/ProductCategoryMapper.java
@@ -6,6 +6,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+// product ctaegory mapper 
 @Mapper(componentModel = "spring", uses = {AttributeMapper.class}) 
 public interface ProductCategoryMapper {
 
