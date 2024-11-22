f214ffc025b2346131b44117bea509200c3af508
Agarvv <casluagarv@gmail.com>
2024-11-22 01:56:00 +0100

    issues

--- server/jagarv/src/main/java/com/app/jagarv/entity/product/AttributeOption.java
+++ server/jagarv/src/main/java/com/app/jagarv/entity/product/AttributeOption.java
@@ -2,7 +2,7 @@ package com.app.jagarv.entity.product;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 @Table(name = "attribute_option")
@@ -19,7 +19,7 @@ public class AttributeOption {
 
     @ManyToOne
     @JoinColumn(name = "attribute_id")
-    @JsonManagedReference
+    @JsonBackReference
     private Attribute attribute;
 
     public Long getId() {
