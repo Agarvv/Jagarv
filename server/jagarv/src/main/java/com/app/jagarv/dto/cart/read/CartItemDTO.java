7d837bd239c1ed75556f325a66a4ad7b32ec09a7
Agarvv <casluagarv@gmail.com>
2024-11-22 14:47:57 +0100

    Please bruhhh

--- server/jagarv/src/main/java/com/app/jagarv/dto/cart/read/CartItemDTO.java
+++ server/jagarv/src/main/java/com/app/jagarv/dto/cart/read/CartItemDTO.java
@@ -2,73 +2,71 @@ package com.app.jagarv.dto.cart.read;
 
 import java.util.List;
 import java.math.BigDecimal; 
-import com.app.jagarv.entity.product.AttributeOption;
-
+import com.app.jagarv.dto.product.read.AttributeOptionDTO;
 
 public class CartItemDTO {
    
-   // product id
-    private Long id;
-    private String title;
-    private BigDecimal price; 
-    private Long quantity;
-    private List<AttributeOption> options;
-    private List<String> pictures; 
+   private Long id; // product id
+   private String title;
+   private BigDecimal price; 
+   private Long quantity;
+   private List<AttributeOptionDTO> options; 
+   private List<String> pictures; 
 
-    public CartItemDTO(Long productId, String title, BigDecimal price, Long quantity, List<AttributeOption> options, List<String> pictures) {
-        this.id = productId;
-        this.title = title;
-        this.price = price; 
-        this.quantity = quantity;
-        this.options = options;
-        this.pictures = pictures; 
-    }
-    
-    public Long getProductId() {
-        return id;
-    }
+   public CartItemDTO(Long productId, String title, BigDecimal price, Long quantity, List<AttributeOptionDTO> options, List<String> pictures) {
+       this.id = productId;
+       this.title = title;
+       this.price = price; 
+       this.quantity = quantity;
+       this.options = options;
+       this.pictures = pictures; 
+   }
+   
+   public Long getProductId() {
+       return id;
+   }
 
-    public void setProductId(Long productId) {
-        this.id = productId;
-    }
+   public void setProductId(Long productId) {
+       this.id = productId;
+   }
 
-    public String getTitle() {
-        return title;
-    }
+   public String getTitle() {
+       return title;
+   }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+   public void setTitle(String title) {
+       this.title = title;
+   }
 
-    public Long getQuantity() {
-        return quantity;
-    }
+   public Long getQuantity() {
+       return quantity;
+   }
 
-    public void setQuantity(Long quantity) {
-        this.quantity = quantity;
-    }
+   public void setQuantity(Long quantity) {
+       this.quantity = quantity;
+   }
 
-    public List<AttributeOption> getOptions() {
-        return options;
-    }
+   public List<AttributeOptionDTO> getOptions() { 
+       return options;
+   }
 
-    public void setOptions(List<AttributeOption> options) {
-        this.options = options;
-    }
-    
-    public BigDecimal getPrice() {
-        return price;
-    }
+   public void setOptions(List<AttributeOptionDTO> options) {
+       this.options = options;
+   }
+   
+   public BigDecimal getPrice() {
+       return price;
+   }
 
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-    
-    public List<String> getPictures() {
-        return pictures; 
-    }
-    
-    public void setPictures(List<String> pictures) {
-        this.pictures = pictures; 
-    }
+   public void setPrice(BigDecimal price) {
+       this.price = price;
+   }
+   
+   public List<String> getPictures() {
+       return pictures; 
+   }
+   
+   public void setPictures(List<String> pictures) {
+       this.pictures = pictures; 
+   }
 }
