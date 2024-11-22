4009489136daf77bbca48e58874a9787e3287bd8
Agarvv <casluagarv@gmail.com>
2024-11-22 14:28:15 +0100

    morr issues

--- server/jagarv/src/main/java/com/app/jagarv/mapper/cart/CartMapper.java
+++ server/jagarv/src/main/java/com/app/jagarv/mapper/cart/CartMapper.java
@@ -1,4 +1,16 @@
-import org.mapstruct.Named;
+package com.app.jagarv.mapper.cart;
+
+import com.app.jagarv.dto.cart.read.CartDTO;
+import com.app.jagarv.dto.cart.read.CartItemDTO;
+import com.app.jagarv.dto.product.read.AttributeOptionDTO;
+import com.app.jagarv.entity.cart.Cart;
+import com.app.jagarv.entity.cart.CartItem;
+import com.app.jagarv.entity.product.AttributeOption;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface CartMapper {
@@ -12,10 +24,9 @@ public interface CartMapper {
     @Mapping(source = "product.id", target = "productId")
     @Mapping(source = "product.title", target = "title")
     @Mapping(source = "product.price", target = "price")
-    @Mapping(source = "options", target = "options", qualifiedByName = "mapOptions")
+    @Mapping(source = "options", target = "options")
     CartItemDTO cartItemToCartItemDTO(CartItem cartItem);
 
-    @Named("mapOptions")
     default List<AttributeOptionDTO> mapOptions(List<AttributeOption> options) {
         return options.stream()
                 .map(option -> new AttributeOptionDTO(
