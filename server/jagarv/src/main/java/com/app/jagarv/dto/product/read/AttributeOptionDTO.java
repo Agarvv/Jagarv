02225160f0d85991e74d523d7bec8a28c504d34b
Agarvv <casluagarv@gmail.com>
2024-11-22 13:49:17 +0100

    issues

--- server/jagarv/src/main/java/com/app/jagarv/dto/product/read/AttributeOptionDTO.java
+++ server/jagarv/src/main/java/com/app/jagarv/dto/product/read/AttributeOptionDTO.java
@@ -1,11 +1,17 @@
 package com.app.jagarv.dto.product.read;
 
-
 public class AttributeOptionDTO {
 
     private Long id;
     private String value;
 
+    public AttributeOptionDTO() {
+    }
+
+    public AttributeOptionDTO(Long id, String value) {
+        this.id = id;
+        this.value = value;
+    }
 
     public Long getId() {
         return id;
--- server/jagarv/src/main/java/com/app/jagarv/dto/user/SetUserProfilePicDTO.java
+++ server/jagarv/src/main/java/com/app/jagarv/dto/user/SetUserProfilePicDTO.java
@@ -1,16 +1,13 @@
 package com.app.jagarv.dto.user; 
 
-public class SetUserProfilePicDTO 
-{
-    private String profilePicture; 
-    
-    public String getProfilePicture() 
-    {
-        return profilePicture; 
+public class SetUserProfilePicDTO {
+    private String profilePicture;
+
+    public String getProfilePicture() {
+        return profilePicture;
     }
-    
-    public void setProfilePicture(String pic)
-    {
-        this.profilePicture = pic; 
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
     }
 }
