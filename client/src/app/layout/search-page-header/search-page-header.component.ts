8fd6e9839da35e11999513d266c588e3de06ad9e
Agarvv <casluagarv@gmail.com>
2024-11-23 00:16:52 +0100

    issues

--- client/src/app/layout/search-page-header/search-page-header.component.ts
+++ client/src/app/layout/search-page-header/search-page-header.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-search-page-header',
@@ -9,9 +9,5 @@ import { Router } from '@angular/router';
 export class SearchPageHeaderComponent {
  @Input() searchQ: string  | null = "";
  
- constructor(private router: Router) {} 
-
- handleSearch(): void {
-   this.router.navigate(['/search', this.searchQ]);
- }
+ constructor() {} 
 }
