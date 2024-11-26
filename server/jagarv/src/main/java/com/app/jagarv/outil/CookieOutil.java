49e627c4f443ff343f8564ee666dbd9f262eb8d0
Agarvv <casluagarv@gmail.com>
2024-11-26 23:31:00 +0100

    issues

--- server/jagarv/src/main/java/com/app/jagarv/outil/CookieOutil.java
+++ server/jagarv/src/main/java/com/app/jagarv/outil/CookieOutil.java
@@ -5,8 +5,8 @@ import jakarta.servlet.http.Cookie;
 public class CookieOutil {
     public static Cookie generateJwtCookie(String jwtToken) {
         Cookie cookie = new Cookie("jwt", jwtToken);
-        cookie.setHttpOnly(true);  
-        cookie.setSecure(true);    
+        cookie.setHttpOnly(true);   
+        cookie.setSecure(true);  
         cookie.setPath("/");       
         cookie.setMaxAge(3600);    
         cookie.setDomain("jagarv-jq5o.onrender.com"); 
--- server/jagarv/src/main/java/com/app/jagarv/repository/order/OrderRepository.java
+++ server/jagarv/src/main/java/com/app/jagarv/repository/order/OrderRepository.java
@@ -9,19 +9,19 @@ import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    //@Query("SELECT COUNT(o) FROM Order o WHERE o.date = :today")
-    //Long getOrdersCountForToday(@Param("today") String today);
+    @Query("SELECT COUNT(o) FROM Order o WHERE o.date = :today")
+    Long getOrdersCountForToday(@Param("today") String today);
 
-   // @Query("SELECT SUM(o.amount) FROM Order o WHERE o.date = :today")
-   // Long getTotalAmountForToday(@Param("today") String today);
+    @Query("SELECT SUM(o.amount) FROM Order o WHERE o.date = :today")
+    Long getTotalAmountForToday(@Param("today") String today);
 
-   // @Query("SELECT o FROM Order o WHERE o.date BETWEEN :start AND :end")
-   // List<Order> getOrdersBetweenDates(@Param("start") String start, @Param("end") String end);
+    @Query("SELECT o FROM Order o WHERE o.date BETWEEN :start AND :end")
+    List<Order> getOrdersBetweenDates(@Param("start") String start, @Param("end") String end);
 
    // @Query("SELECT p.title, SUM(oi.quantity), p.price, p.stock " +
-    //       "FROM Order o JOIN o.orderItems oi JOIN oi.product p " +
-   //        "GROUP BY p.title, p.price, p.stock")
-   // List<Object[]> getMostOrderedProducts(); 
+  //        "FROM Order o JOIN o.orderItems oi JOIN oi.product p " +
+ //         "GROUP BY p.title, p.price, p.stock")
+ //   List<Object[]> getMostOrderedProducts(); 
 
     List<Order> findAllByUser_Id(Long userId);
 
