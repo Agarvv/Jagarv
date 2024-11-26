1d889cc32ec45a26a0bbd08a3de7946582453761
Agarvv <casluagarv@gmail.com>
2024-11-26 22:31:15 +0100

    switching to local to debug

--- client/src/app/environments/environment.ts
+++ client/src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://jagarv-jq5o.onrender.com',
+  apiUrl: 'http://localhost:8080',
   stripePublicKey: 'pk_test_51QLORxG0ugv4veBG48CWA8Eo1gb9dHDL13El35Fbub17pbv3pzUBU7g9e60QCQ3U4BgrNnPmMSVHVIrKnjATr0Yv00d7aCtVxX',
   cloudinary: {
       cloud_name: 'dtlpkq8w3',
