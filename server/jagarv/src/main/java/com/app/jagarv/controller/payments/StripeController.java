73392331f0564f9ff698d6e90c61780de076095e
Agarvv <casluagarv@gmail.com>
2024-12-01 16:58:30 +0100

    bad webhook url

--- server/jagarv/src/main/java/com/app/jagarv/controller/payments/StripeController.java
+++ server/jagarv/src/main/java/com/app/jagarv/controller/payments/StripeController.java
@@ -35,7 +35,7 @@ public class StripeController
         }
     }
 
-    @PostMapping("/stripe/webhook")
+    @PostMapping("/webhook")
 public ResponseEntity<String> handleStripeWebhook(HttpServletRequest request) {
     try {
         sendMail.sendMail("casluagarv@gmail.com", 
