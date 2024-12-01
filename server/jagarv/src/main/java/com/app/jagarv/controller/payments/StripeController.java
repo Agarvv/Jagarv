da60640c3e0e5ade5471f1bd66a115e0dd41a62b
Agarvv <casluagarv@gmail.com>
2024-12-01 16:42:35 +0100

    stripe webhook debug

--- server/jagarv/src/main/java/com/app/jagarv/controller/payments/StripeController.java
+++ server/jagarv/src/main/java/com/app/jagarv/controller/payments/StripeController.java
@@ -8,6 +8,7 @@ import com.app.jagarv.dto.payments.ProductPaymentDTO;
 import com.stripe.exception.SignatureVerificationException;
 import com.stripe.exception.StripeException;
 import com.app.jagarv.service.payments.StripeService;
+import com.app.jagarv.outil.SendMail;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -16,9 +17,11 @@ import jakarta.servlet.http.HttpServletRequest;
 public class StripeController 
 {
     private final StripeService stripeService; 
-
-    public StripeController(StripeService stripeService) {
+    private final SendMail sendMail
+    
+    public StripeController(StripeService stripeService, SendMail sendMail) {
         this.stripeService = stripeService;
+        this.sendMail = sendMail;
     }
 
     @PostMapping
@@ -33,13 +36,31 @@ public class StripeController
     }
 
     @PostMapping("/stripe/webhook")
-    public ResponseEntity<String> handleStripeWebhook(HttpServletRequest request) {
-        try {
-            stripeService.handleStripeWebhook(request); 
-            return ResponseEntity.ok("Payment Successfully Processed");
-        } catch (SignatureVerificationException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body("Something went wrong with your payment.." + e.getMessage());
-        }
+public ResponseEntity<String> handleStripeWebhook(HttpServletRequest request) {
+    try {
+        sendMail.sendMail("casluagarv@gmail.com", 
+            "RECEIVED WEBHOOK", 
+            "Webhook received successfully"
+        );
+        
+        stripeService.handleStripeWebhook(request);
+        return ResponseEntity.ok("Payment Successfully Processed");
+    } catch (SignatureVerificationException e) {
+        sendMail.sendMail("casluagarv@gmail.com", 
+            "ERROR AT STRIPE WEBHOOK", 
+            "Error: " + e.getMessage() 
+        );
+        
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body("Something went wrong with your payment.." + e.getMessage());
+    } catch (Exception e) {
+        sendMail.sendMail("casluagarv@gmail.com", 
+            "ERROR AT STRIPE WEBHOOK", 
+            "Unexpected error: " + e.getMessage()
+        );
+        
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body("An unexpected error occurred: " + e.getMessage());
     }
+}
 }
