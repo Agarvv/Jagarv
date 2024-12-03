85cdad3ac4e6dac384ff4f899d2b9a639b9ec2a2
Agarvv <casluagarv@gmail.com>
2024-12-03 09:45:14 +0100

    issues

--- server/jagarv/src/main/java/com/app/jagarv/repository/order/OrderRepository.java
+++ server/jagarv/src/main/java/com/app/jagarv/repository/order/OrderRepository.java
@@ -3,9 +3,6 @@ package com.app.jagarv.repository.order;
 import com.app.jagarv.entity.order.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import com.app.jagarv.entity.product.Product;
-import com.app.jagarv.entity.order.Order;
 
 import java.util.List;
 
@@ -17,21 +14,21 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query(value = "SELECT SUM(amount) FROM orders WHERE date::DATE = CURRENT_DATE", nativeQuery = true)
     Long getTotalEarningsToday();
 
-    @Query(value = "SELECT p.* FROM products p " +
-               "JOIN order_product op ON op.product_id = p.id " +
-               "JOIN orders o ON o.id = op.order_id " +
-               "GROUP BY p.id ORDER BY COUNT(o.id) DESC", nativeQuery = true)
-    List<Product> findMostOrderedProducts();
-    
+    @Query(value = "SELECT p.id, p.name, COUNT(o.id) AS order_count " +
+                   "FROM products p " +
+                   "JOIN order_product op ON op.product_id = p.id " +
+                   "JOIN orders o ON o.id = op.order_id " +
+                   "GROUP BY p.id " +
+                   "ORDER BY order_count DESC", nativeQuery = true)
+    List<Object[]> findMostOrderedProducts();
+
     @Query(value = "SELECT EXTRACT(MONTH FROM date::DATE) AS month, COUNT(*) AS order_count " +
-               "FROM orders " +
-               "WHERE EXTRACT(MONTH FROM date::DATE) BETWEEN 7 AND 12 " +
-               "GROUP BY month " +
-               "ORDER BY month", nativeQuery = true)
+                   "FROM orders " +
+                   "WHERE EXTRACT(MONTH FROM date::DATE) BETWEEN 7 AND 12 " +
+                   "GROUP BY month " +
+                   "ORDER BY month", nativeQuery = true)
     List<Object[]> getOrderCountByMonth();
-   
-    
-    
+
     List<Order> findAllByUser_Id(Long userId);
 
     Boolean existsByUserIdAndProducts_Id(Long userId, Long productId);
