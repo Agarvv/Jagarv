273757c6bcfc064fc3399e85fef98c59b577bd8c
Agarvv <casluagarv@gmail.com>
2024-12-03 11:08:22 +0100

    issues

--- server/jagarv/src/main/java/com/app/jagarv/entity/order/Order.java
+++ server/jagarv/src/main/java/com/app/jagarv/entity/order/Order.java
@@ -25,11 +25,11 @@ public class Order {
 
     @ManyToMany
     @JoinTable(
-        name = "order_product", 
-        joinColumns = @JoinColumn(name = "order_id"), 
-        inverseJoinColumns = @JoinColumn(name = "product_id")
-    )
-    private List<CartItem> products;
+    name = "order_product", 
+    joinColumns = @JoinColumn(name = "order_id"), 
+    inverseJoinColumns = @JoinColumn(name = "cart_item_id")  
+     )
+     private List<CartItem> products;
 
     // @NotBlank(message = "Status cannot be blank")
    // @Size(max = 20, message = "Status cannot exceed 20 characters")
