3837dde5fdb504e3eac7401947559edbf30dc5ff
Agarvv <casluagarv@gmail.com>
2024-12-03 09:35:09 +0100

    issues

--- server/jagarv/src/main/java/com/app/jagarv/controller/admin/dashboard/AdminDashboardController.java
+++ server/jagarv/src/main/java/com/app/jagarv/controller/admin/dashboard/AdminDashboardController.java
@@ -1,10 +1,15 @@
+package com.app.jagarv.controller.admin.dashboard;
+
+import com.app.jagarv.service.admin.dashboard.AdminDashboardService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin/dashboard")
 public class AdminDashboardController {
+
     private final AdminDashboardService adminDashboardService;
 
     public AdminDashboardController(AdminDashboardService adminDashboardService) {
--- server/jagarv/src/main/java/com/app/jagarv/service/admin/dashboard/AdminDashboardService.java
+++ server/jagarv/src/main/java/com/app/jagarv/service/admin/dashboard/AdminDashboardService.java
@@ -1,16 +1,8 @@
 package com.app.jagarv.service.admin.dashboard;
 
-import com.app.jagarv.dto.admin.dashboard.AdminDashboardDTO;
-import com.app.jagarv.dto.product.read.MostOrderedProductDTO;
-import com.app.jagarv.dto.sales.read.MonthlySalesDTO;
 import com.app.jagarv.repository.order.OrderRepository;
 import com.app.jagarv.repository.user.UserRepository;
-import com.app.jagarv.mapper.admin.AdminDashboardMapper;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,7 +19,7 @@ public class AdminDashboardService {
     }
 
     public Map<String, Object> getAdminDashboard() {
-        Map<String, Object> dashboardData = new HashMap<>(); // just for now 
+        Map<String, Object> dashboardData = new HashMap<>();
         dashboardData.put("ordersToday", orderRepository.findOrdersToday());
         dashboardData.put("totalEarningsToday", orderRepository.getTotalEarningsToday());
         dashboardData.put("mostOrderedProducts", orderRepository.findMostOrderedProducts());
