de556ff8cfd0fe2aa4904f2555c83d205901c8d4
Agarvv <casluagarv@gmail.com>
2024-12-04 22:37:23 +0100

    issues

--- server/jagarv/src/main/java/com/app/jagarv/repository/order/OrderRepository.java
+++ server/jagarv/src/main/java/com/app/jagarv/repository/order/OrderRepository.java
@@ -4,6 +4,8 @@ import com.app.jagarv.entity.order.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import com.app.jagarv.dto.product.read.BestSellerDTO;
+import org.springframework.data.repository.query.Param;
+
 
 import java.util.List;
 
