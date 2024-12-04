c18813e4cc8fd462e6cdc38451993437142bffe9
Agarvv <casluagarv@gmail.com>
2024-12-04 02:59:41 +0100

    issues

--- client/src/app/models/admin/dashboard/AdminDashboard.ts
+++ client/src/app/models/admin/dashboard/AdminDashboard.ts
@@ -1,9 +1,8 @@
-
 export interface AdminDashboard {
-  orderCountByMonth: [number, number][]; // first number is month, and second is the number of orders.
+  orderCountByMonth: [number, number][]; 
   usersToday: number;
   ordersToday: number;
-  mostOrderedProducts: [number, string, number][];
+  mostOrderedProducts: [number, string, string, number, number, number][]; 
   totalEarningsToday: number;
   totalIncome: number;
 }
