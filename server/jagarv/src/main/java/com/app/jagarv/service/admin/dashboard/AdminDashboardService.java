b6b8973a1dcdd3df93e4ffa1e7b338d72280d4cd
Agarvv <casluagarv@gmail.com>
2024-12-04 20:37:29 +0100

    issues

--- server/jagarv/src/main/java/com/app/jagarv/service/admin/dashboard/AdminDashboardService.java
+++ server/jagarv/src/main/java/com/app/jagarv/service/admin/dashboard/AdminDashboardService.java
@@ -1,10 +1,14 @@
 package com.app.jagarv.service.admin.dashboard;
 
+import com.app.jagarv.dto.product.read.BestSellerDTO;
 import com.app.jagarv.repository.order.OrderRepository;
 import com.app.jagarv.repository.user.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -24,8 +28,21 @@ public class AdminDashboardService {
         dashboardData.put("ordersToday", orderRepository.countOrdersToday()); 
         
         dashboardData.put("totalEarningsToday", orderRepository.getTotalEarningsToday()); 
+        List<BestSellerDTO> bestSellersDTO = new ArrayList<>();
+        List<Object[]> mostOrderedProducts = orderRepository.findMostOrderedProducts(); 
+        for(Object[] mostOrdered: mostOrderedProducts) {
+            Long productId = (Long) mostOrdered[0];
+            String title = (String) mostOrdered[1];
+            String pictures = (String) mostOrdered[2];
+            Long stock = (Long) mostOrdered[3];
+            BigDecimal price = (BigDecimal) mostOrdered[4];
+            Long selled = (Long) mostOrdered[5];
+            String[] picturesArray = pictures.split(",");  
+            BestSellerDTO dto = new BestSellerDTO(productId, title, picturesArray, stock, price, selled);
+            bestSellersDTO.add(dto);
+        }
         
-        dashboardData.put("mostOrderedProducts", orderRepository.findMostOrderedProducts()); 
+        dashboardData.put("mostOrderedProducts", bestSellersDTO); 
         
         dashboardData.put("orderCountByMonth", orderRepository.getOrderCountByMonth());
         
