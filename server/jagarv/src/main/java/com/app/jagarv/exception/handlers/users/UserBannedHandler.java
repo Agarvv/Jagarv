2f6d3474b4a5c9e67f9fb312b3732fecaa6c89f6
Agarvv <casluagarv@gmail.com>
2024-12-06 13:40:16 +0100

    issues

--- server/jagarv/src/main/java/com/app/jagarv/exception/handlers/users/UserBannedHandler.java
+++ server/jagarv/src/main/java/com/app/jagarv/exception/handlers/users/UserBannedHandler.java
@@ -5,14 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.app.jagarv.exception.exceptions.users.UserBannedException; 
+import com.app.jagarv.exception.exceptions.users.UserBannedException;
 
 @ControllerAdvice
 public class UserBannedHandler {
-    @ExceptionHandler(UserBannedException.class) {
-        public ResponseEntity<String>
-        handleUserBannedException(UserBannedException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
-        }
+    
+    @ExceptionHandler(UserBannedException.class)
+    public ResponseEntity<String> handleUserBannedException(UserBannedException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
 }
