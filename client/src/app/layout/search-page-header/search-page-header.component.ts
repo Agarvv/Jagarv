7ed83bda25f8d513571a271d9cb031a1cb58e145
Agarvv <casluagarv@gmail.com>
2024-12-06 12:22:56 +0100

    issues

--- client/src/app/layout/search-page-header/search-page-header.component.ts
+++ client/src/app/layout/search-page-header/search-page-header.component.ts
@@ -1,19 +1,26 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-page-header',
   templateUrl: './search-page-header.component.html',
-  styleUrl: './search-page-header.component.css'
+  styleUrls: ['./search-page-header.component.css']
 })
 export class SearchPageHeaderComponent {
- @Input() searchQ: string  | null = "";
- @Output() showFilters: EventEmitter<void> = new EventEmitter();
+  @Input() searchQ: string  | null = "";
+  @Output() showFilters: EventEmitter<void> = new EventEmitter();
 
- 
- constructor() {} 
+  constructor(private router: Router) {}
 
- showFiltersAside() {
-  this.showFilters.emit();
- }
+  reloadPage() {
+    if (this.searchQ) {
+      this.router.navigate(['/search', this.searchQ]).then(() => {
+        window.location.reload();  
+      });
+    }
+  }
+
+  showFiltersAside() {
+    this.showFilters.emit();
+  }
 }
